1d5ff01b071f1958ebb17bf8fabb9b4a42695433
Peter Bräuer <pb866.git@gmail.com>
2025-08-21 20:55:19 +0200

    Update changelog and documentation
    Signed-off-by: Peter Bräuer <pb866.git@gmail.com>

--- src/SFTP.jl
+++ src/SFTP.jl
@@ -1,6 +1,28 @@
 module SFTP
 
 # Error Codes for Error Handling of IO Errors
+"""
+# Enum `ErrorCode`
+
+Error codes for SFTP connection and remote filesystem errors:
+
+    EC_NOERROR = 0
+    EC_ACCESS_DENIED = 1
+    EC_NOT_A_FILE = 2
+    EC_NOT_A_DIR = 3
+    EC_NOT_A_PATH = 4
+    EC_FILE_NOT_FOUND = 5
+    EC_DIR_NOT_FOUND = 6
+    EC_PATH_NOT_FOUND = 7
+    EC_FILE_EXISTS = 8
+    EC_DIR_EXISTS = 9
+    EC_PATH_EXISTS = 10
+    EC_NONEMPTY_DIR = 11
+    EC_BROKEN_LINK = 12
+    EC_INVALID_SCAN = 13
+"""
+ErrorCode
+
 @enum ErrorCode begin
     EC_NOERROR = 0
     EC_ACCESS_DENIED = 1
